b32d6668f444e06c967f400c276b6ce018adc741
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-12-09 15:45:21 +0000

    The grid contains numbers

--- sudoku.rb
+++ sudoku.rb
@@ -1,4 +1,16 @@
 require 'sinatra'
+require_relative './lib/sudoku'
+require_relative './lib/cell'
+
+def random_sudoku
+	seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+	sudoku = Sudoku.new(seed.join)
+	sudoku.solve!
+	sudoku.to_s.chars
+end
+
+
 get '/' do
-	"Hello Sudoku"
+	@current_solution = random_sudoku
+	erb :index
 end

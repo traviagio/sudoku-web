d3bf9473a221d2e124bb62812c22d539cdd7e060
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-12-10 14:09:43 +0000

    solution saved in a session

--- sudoku.rb
+++ sudoku.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
+enable :sessions
+
 def random_sudoku
 	seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
 	sudoku = Sudoku.new(seed.join)
@@ -9,8 +11,19 @@ def random_sudoku
 	sudoku.to_s.chars
 end
 
+def puzzle(sudoku)
+	sudoku
+end
+
 
 get '/' do
-	@current_solution = random_sudoku
+	sudoku = random_sudoku
+	session[:solution] = sudoku
+	@current_solution = puzzle(sudoku)
+	erb :index
+end
+
+get '/solution' do
+	@current_solution = session[:solution]
 	erb :index
 end

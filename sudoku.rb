98817fa551f0a0d52e880a315ef9525f82c18e80
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-12-12 16:59:20 +0000

    separated the helper

--- sudoku.rb
+++ sudoku.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
+require_relative './helpers/application'
 
 enable :sessions
 
@@ -48,30 +49,6 @@ def box_order_to_row_order(cells)
   
     memo += three_rows_of_three.flatten
   }
-end
-
-
-
-helpers do
-   def cell_value(value)
-    value.to_i == 0 ? '' : value
-  end
-
-  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
-    must_be_guessed = puzzle_value == 0
-    #I needed to change this 0 to "0" otherwise all the cells show up as value provided
-    tried_to_guess = current_solution_value.to_i != 0
-    guessed_incorrectly = current_solution_value != solution_value
-
-    if solution_to_check && 
-        must_be_guessed && 
-        tried_to_guess && 
-        guessed_incorrectly
-      'incorrect'
-    elsif !must_be_guessed
-      'value-provided'
-    end
-  end
 end
 
 

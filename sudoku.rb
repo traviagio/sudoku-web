35e42881d4169ef6e9b7cb2b3bf89114e96cf628
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-12-12 15:40:47 +0000

    from box order to row order method

--- sudoku.rb
+++ sudoku.rb
@@ -12,7 +12,11 @@ def random_sudoku
 end
 
 def puzzle(sudoku)
-	sudoku
+	numbers = random_sudoku
+	until numbers.count("") == 41
+		numbers[rand(0..80)] = ""
+	end
+	numbers
 end
 
 
@@ -26,4 +30,22 @@ end
 get '/solution' do
 	@current_solution = session[:solution]
 	erb :index
+end
+
+def box_order_to_row_order(cells)
+
+  boxes = cells.each_slice(9).to_a
+  (0..8).to_a.inject([]) {|memo, i|
+
+    first_box_index = i / 3 * 3
+    three_boxes = boxes[first_box_index, 3]
+    three_rows_of_three = three_boxes.map do |box| 
+
+      row_number_in_a_box = i % 3
+      first_cell_in_the_row_index = row_number_in_a_box * 3
+      box[first_cell_in_the_row_index, 3]
+    end
+  
+    memo += three_rows_of_three.flatten
+  }
 end
